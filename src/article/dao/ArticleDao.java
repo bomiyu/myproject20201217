b80b5133fd7db7b0edf9438da0375759d08b65cd
User <bom7768@gmail.com>
2020-12-29 00:17:38 +0900

    게시글목록에 댓글갯수 보여지지만 글쓰기 동작 시 리스트에 보여지지않음

--- src/article/dao/ArticleDao.java
+++ src/article/dao/ArticleDao.java
@@ -14,36 +14,31 @@ import article.model.Writer;
 import jdbc.JdbcUtil;
 
 public class ArticleDao {
-	
+
 	public int update(Connection conn, int no, String title) throws SQLException {
-		String sql = "UPDATE article "
-				+ "SET title=?, moddate=SYSDATE "
-				+ "WHERE article_no=?";
-		
+		String sql = "UPDATE article " + "SET title=?, moddate=SYSDATE " + "WHERE article_no=?";
+
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setString(1, title);
 			pstmt.setInt(2, no);
-			
+
 			int cnt = pstmt.executeUpdate();
 			return cnt;
 		}
 	}
-	
-	
+
 	public Article selectById(Connection conn, int no) throws SQLException {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "SELECT * "
-				+ "FROM article "
-				+ "WHERE article_no=?";
-		
+		String sql = "SELECT * " + "FROM article " + "WHERE article_no=?";
+
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, no);
 			rs = pstmt.executeQuery();
-			
+
 			Article article = null;
-			
+
 			if (rs.next()) {
 				article = convertArticle(rs);
 			}
@@ -51,114 +46,79 @@ public class ArticleDao {
 		} finally {
 			JdbcUtil.close(rs, pstmt);
 		}
-		
+
 	}
-	
+
 	public void increaseReadCount(Connection conn, int no) throws SQLException {
-		String sql = "UPDATE article "
-				+ "SET read_cnt=read_cnt+1 "
-				+ "WHERE article_no=?";
+		String sql = "UPDATE article " + "SET read_cnt=read_cnt+1 " + "WHERE article_no=?";
 		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, no);
 			pstmt.executeUpdate();
 		}
 	}
-	
-	
-	public List<Article> select(Connection conn, int pageNum, int size) 
-		throws SQLException {
-		
+
+	public List<Article> select(Connection conn, int pageNum, int size) throws SQLException {
+
 		/*
-		String sql = "SELECT * "
-				+ "FROM article "
-				+ "ORDER BY articl_no DESC "
-				+ "LIMIT ?, ?"; // 시작 row_num(zerobase), 갯수
-		*/
-	
-		String sql = "SELECT "
-				+ "rn, "
-				+ "article_no, "
-				+ "writer_id, "
-				+ "writer_name, "
-				+ "title, "
-				+ "regdate, "
-				+ "moddate, "
-				+ "read_cnt "
-				+ "FROM ("
-				+ "	SELECT article_no, "
-				+ " 		   writer_id, "
-				+ "        writer_name, "
-				+ "        title, "
-				+ "        regdate, "
-				+ "        moddate, "
-				+ "        read_cnt, "
-				+ "        ROW_NUMBER() "
-				+ "          OVER ( "
-				+ "            ORDER BY "
-				+ "            article_no "
-				+ "            DESC) "
-				+ "        rn "
-				+ "  FROM article "
-				+ ") WHERE rn "
-				+ "    BETWEEN ? AND ?";
-	
-		/*
-		 * String sql = "SELECT " + "a.rn, " + "a.article_no, " + "writer_id, " +
-		 * "writer_name, " + "title, " + "regdate, " + "moddate, " + "read_cnt," +
-		 * "b.reply_cnt " + "FROM (" + "	SELECT article_no, " +
-		 * " 		   writer_id, " + "        writer_name, " + "        title, " +
-		 * "        regdate, " + "        moddate, " + "        read_cnt, " +
-		 * "        ROW_NUMBER() " + "          OVER ( " + "            ORDER BY " +
-		 * "            article_no " + "            DESC) " + "        rn " +
-		 * "  FROM article " + ") a, " +
-		 * " (SELECT article_no, count(*) reply_cnt FROM reply GROUP BY article_no) b "
-		 * + " WHERE a.article_no = b.article_no AND " + "       rn BETWEEN ? AND ? " +
-		 * " ORDER BY a.rn DESC ";
+		 * String sql = "SELECT * " + "FROM article " + "ORDER BY articl_no DESC " +
+		 * "LIMIT ?, ?"; // 시작 row_num(zerobase), 갯수
 		 */
+		/*
+		 * String sql = "SELECT " + "rn, " + "article_no, " + "writer_id, " +
+		 * "writer_name, " + "title, " + "regdate, " + "moddate, " + "read_cnt " +
+		 * "FROM (" + "	SELECT article_no, " + " 		   writer_id, " +
+		 * "        writer_name, " + "        title, " + "        regdate, " +
+		 * "        moddate, " + "        read_cnt, " + "        ROW_NUMBER() " +
+		 * "          OVER ( " + "            ORDER BY " + "            article_no " +
+		 * "            DESC) " + "        rn " + "  FROM article " + ") WHERE rn " +
+		 * "    BETWEEN ? AND ?";
+		 */
+
+		String sql = "SELECT " + "a.rn, " + "a.article_no, " + "writer_id, " + "writer_name, " + "title, " + "regdate, "
+				+ "moddate, " + "read_cnt," + "b.reply_cnt " + "FROM (" + "	SELECT article_no, "
+				+ " 		   writer_id, " + "        writer_name, " + "        title, " + "        regdate, "
+				+ "        moddate, " + "        read_cnt, " + "        ROW_NUMBER() " + "          OVER ( "
+				+ "            ORDER BY " + "            article_no " + "            DESC) " + "        rn "
+				+ "  FROM article " + ") a, "
+				+ " (SELECT article_no, count(*) reply_cnt FROM reply GROUP BY article_no) b "
+				+ " WHERE a.article_no = b.article_no AND " + "       rn BETWEEN ? AND ? " + " ORDER BY a.rn DESC ";
+
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, (pageNum-1) * size + 1);
+			pstmt.setInt(1, (pageNum - 1) * size + 1);
 			pstmt.setInt(2, pageNum * size);
-			
+
 			rs = pstmt.executeQuery();
 			List<Article> result = new ArrayList<>();
 			while (rs.next()) {
 				result.add(convertArticle(rs));
 			}
-			
+
 			return result;
 		} finally {
 			JdbcUtil.close(rs, pstmt);
 		}
 	}
-	
+
 	private Article convertArticle(ResultSet rs) throws SQLException {
-		return new Article(rs.getInt("article_no"),
-					new Writer(
-							rs.getString("writer_id"),
-							rs.getString("writer_name")
-							),
-					rs.getString("title"),
-					rs.getTimestamp("regdate"),
-					rs.getTimestamp("moddate"),
-					rs.getInt("read_cnt")
-//					rs.getInt("reply_cnt")
-				); 
+		return new Article(rs.getInt("article_no"), new Writer(rs.getString("writer_id"), rs.getString("writer_name")),
+				rs.getString("title"), rs.getTimestamp("regdate"), rs.getTimestamp("moddate"), rs.getInt("read_cnt"),
+				rs.getInt("reply_cnt"));
 	}
-	
+
 	public int selectCount(Connection conn) throws SQLException {
 		String sql = "SELECT COUNT(*) FROM article";
-		
+
 		Statement stmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-			
+
 			if (rs.next()) {
 				return rs.getInt(1);
 			}
@@ -167,37 +127,32 @@ public class ArticleDao {
 			JdbcUtil.close(rs, stmt);
 		}
 	}
-	
-	
-	public Article insert(Connection conn, Article article) 
-			throws SQLException {
+
+	public Article insert(Connection conn, Article article) throws SQLException {
 		// 12c 이상
-		String sql = "INSERT INTO article "
-				+ "(writer_id, writer_name, title,"
-				+ " regdate, moddate, read_cnt) "
+		String sql = "INSERT INTO article " + "(writer_id, writer_name, title," + " regdate, moddate, read_cnt) "
 				+ "VALUES (?, ?, ?, SYSDATE, SYSDATE, 0)";
-		
+
 		// 11g
 		/*
-		String sql = "INSERT INTO article "
-				+ "(article_no, writer_id, writer_name, title,"
-				+ " regdate, moddate, read_cnt) "
-				+ "VALUES (article_seq.nextval, ?, ?, ?, SYSDATE, SYSDATE, 0)";
-				*/
-		
+		 * String sql = "INSERT INTO article " +
+		 * "(article_no, writer_id, writer_name, title," +
+		 * " regdate, moddate, read_cnt) " +
+		 * "VALUES (article_seq.nextval, ?, ?, ?, SYSDATE, SYSDATE, 0)";
+		 */
+
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
-			pstmt = conn.prepareStatement(sql,
-							new String[] {"article_no", "regdate", "moddate"});
-			
+			pstmt = conn.prepareStatement(sql, new String[] { "article_no", "regdate", "moddate" });
+
 			pstmt.setString(1, article.getWriter().getId());
 			pstmt.setString(2, article.getWriter().getName());
 			pstmt.setString(3, article.getTitle());
-			
+
 			int cnt = pstmt.executeUpdate();
-			
+
 			if (cnt == 1) {
 				rs = pstmt.getGeneratedKeys();
 				int key = 0;
@@ -208,12 +163,7 @@ public class ArticleDao {
 					regDate = rs.getTimestamp(2);
 					modDate = rs.getTimestamp(3);
 				}
-				return new Article(key,
-						article.getWriter(),
-						article.getTitle(),
-						regDate,
-						modDate,
-						0);
+				return new Article(key, article.getWriter(), article.getTitle(), regDate, modDate, 0);
 			} else {
 				return null;
 			}
@@ -222,18 +172,13 @@ public class ArticleDao {
 		}
 	}
 
-
 	public void delete(Connection con, int no) throws SQLException {
 		String sql = "DELETE article WHERE article_no=?";
-		
+
 		try (PreparedStatement pstmt = con.prepareStatement(sql)) {
 			pstmt.setInt(1, no);
-			
+
 			pstmt.executeUpdate();
 		}
 	}
 }
-
-
-
-
--- src/article/model/Article.java
+++ src/article/model/Article.java
@@ -9,8 +9,8 @@ public class Article {
 	private Date regDate;
 	private Date modifiedDate;
 	private int readCount;
-//	private int replyCnt;
-	
+	private int replyCnt;
+
 	public Article(Integer number, Writer writer, String title, Date regDate, Date modifiedDate, int readCount) {
 		super();
 		this.number = number;
@@ -20,35 +20,45 @@ public class Article {
 		this.modifiedDate = modifiedDate;
 		this.readCount = readCount;
 	}
-	
-	/*
-	 * public Article(Integer number, Writer writer, String title, Date regDate,
-	 * Date modifiedDate, int readCount, int replyCnt) { super(); this.number =
-	 * number; this.writer = writer; this.title = title; this.regDate = regDate;
-	 * this.modifiedDate = modifiedDate; this.readCount = readCount; this.replyCnt =
-	 * replyCnt; }
-	 * 
-	 * public int getReplyCnt() { return replyCnt; }
-	 */
-	
+
+	public Article(Integer number, Writer writer, String title, Date regDate, Date modifiedDate, int readCount,
+			int replyCnt) {
+		super();
+		this.number = number;
+		this.writer = writer;
+		this.title = title;
+		this.regDate = regDate;
+		this.modifiedDate = modifiedDate;
+		this.readCount = readCount;
+		this.replyCnt = replyCnt;
+	}
+
+	public int getReplyCnt() {
+		return replyCnt;
+	}
+
 	public Integer getNumber() {
 		return number;
 	}
+
 	public Writer getWriter() {
 		return writer;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public Date getRegDate() {
 		return regDate;
 	}
+
 	public Date getModifiedDate() {
 		return modifiedDate;
 	}
+
 	public int getReadCount() {
 		return readCount;
 	}
-	
-	
+
 }
